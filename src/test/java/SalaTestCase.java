589eb8b343b19776933bdd98c54989712103ad2c
Bernardo Neto <2230783@my.ipleiria.pt>
2025-06-21 14:37:27 +0100

    Sala variables changed

--- src/test/java/SalaTestCase.java
+++ src/test/java/SalaTestCase.java
@@ -5,14 +5,14 @@ public class SalaTestCase {
 
     @Test
     public void testSalaNomeChange() {
-        Sala sala = new Sala("A1", 100, "Dolby", "IMAX", "Reclináveis", "Ar condicionado");
+        Sala sala = new Sala("A1", 50,50, "Dolby", "IMAX", "Reclináveis", "Ar condicionado");
         sala.setNome("A2");
         assertEquals("A2", sala.getNome());
     }
 
     @Test
     public void testSalaIsActiveByDefault() {
-        Sala s = new Sala("A3", 150, "Dolby", "Normal", "Fixos", "Nenhuma");
+        Sala s = new Sala("A3", 100,50, "Dolby", "Normal", "Fixos", "Nenhuma");
         assertTrue(s.isAtiva());
     }
 }
